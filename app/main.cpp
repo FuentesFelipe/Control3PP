e22394acb41796a6212b513b0c418b4065e6d7d2
FuentesFelipe <f.fuentes.pro@gmail.com>
2019-05-25 23:38:01 -0400

    Paralelizado, pero algo no funciona

--- app/main.cpp
+++ app/main.cpp
@@ -1,67 +1,72 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <cstdlib>
 #include <fstream>
 #include "menu.h"
 #include "Matriz.h"
 #include <mpi.h>
+#include <time.h>
 
 int main(int argc, char** argv) {
 
-  MPI_Init(&argc, &argv);
-
-  int world_size;
-    // Get the number of processes
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-
-    // Finalize the MPI environment.
-    MPI_Finalize();
-
-    if (argc > 4)
+    if (argc > 4) {
         std::string archivo_entrada(obtener_entrada(argv, argc));
         std::string archivo_salida(obtener_salida(argv, argc));
 
         if (!archivo_entrada.empty() && !archivo_salida.empty()) {
 
-            std::ifstream entrada(archivo_entrada);
-            std::ofstream salida(archivo_salida);
-            std::string linea;
-            while (std::getline(entrada, linea)) {
-                Matriz matriz(linea);
-                if (matriz.esMagico()) {
-                    std::string str = matriz.toString();
-                    salida.write(str.c_str(), str.size());
+              int TAG = 0;                        // TAG
+              int procesador = 0;
+              int tamano = 0;
+
+              MPI_Init(NULL, NULL);
+
+              MPI_Comm_rank(MPI_COMM_WORLD, &procesador);
+              MPI_Comm_size(MPI_COMM_WORLD, &tamano);
+
+              std::ifstream entrada(archivo_entrada);
+              std::ofstream salida(archivo_salida);
+              std::string linea;
+
+              while (std::getline(entrada, linea)) {
+                // Procesador maestro
+                if(procesador == 0){
+                    MPI_Status status;
+
+                  for(int fuente = 1; fuente < tamano; fuente++){
+                      std::string linea;
+                      MPI_Recv(&linea,linea.size(), MPI_CHAR,fuente, TAG, MPI_COMM_WORLD, &status);
+                      salida.write(linea.c_str(), linea.size());
+                      std::cerr << std::endl << "Recibiendo desde "<<fuente<<std::endl;
+                      }
                 }
+                else{
+                    for(int i = 0; i < tamano; i++){
+                        Matriz matriz(linea);
+                        if (matriz.esMagico()) {
+                            std::string str = matriz.toString();
+                            MPI_Send(&str,str.size(), MPI_CHAR, 0, 7, MPI_COMM_WORLD);
+                            }
+                        linea.clear();
+                        }
+                    }
+              } // while
+                  MPI_Finalize();
+                  entrada.close();
+                  salida.close();
 
-                linea.clear();
-            }
-
-            entrada.close();
-            salida.close();
 
             return EXIT_SUCCESS;
         } else {
             std::cerr << std::endl << "Error al determinar las rutas de los archivos de entrada " << archivo_entrada << " y salida " << archivo_salida << std::endl;
             return EXIT_FAILURE;
-        }
-    } else {
+
+          }
+    }
+      else {
         std::cerr << std::endl << "Argumentos insuficientes" << std::endl;
         std::cerr << std::endl << argv[0] << " -f /ruta/archivo/entrada.txt -o /ruta/archivo/salida.txt" << std::endl;
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
-}
+  }
